1bc0ba1021b317434e0ad0206f3513b5448bc8c9
laurent <laurent.picard.LP2@gmail.com>
2019-03-21 22:49:03 +0100

    creation of chart.js

--- src/app/booking/timestamp-facility/timestamp-facility.component.ts
+++ src/app/booking/timestamp-facility/timestamp-facility.component.ts
@@ -5,6 +5,7 @@ import { Command } from 'src/app/models/command.model';
 import { TimestampFacility } from 'src/app/models/timestamp-facility.model';
 import { Seance } from 'src/app/models/seance.model';
 import { SeanceService } from 'src/app/services/seance.service';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-timestamp-facility',
@@ -16,12 +17,53 @@ export class TimestampFacilityComponent implements OnInit {
   timestampFacilities: TimestampFacility[]
   seance: Seance;
   command: Command;
+  chart;
   
 
   constructor(private commandService: CommandService,
               private seanceService: SeanceService) { }
 
   ngOnInit() {
+
+    this.chart = new Chart('myChart', {
+      type: 'bar',
+      data: {
+        labels: ["Janvier", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: "rgba(75,192,192,0.4)",
+                borderColor: "rgba(75,192,192,1)",
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: "rgba(75,192,192,1)",
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                pointHoverBorderColor: "rgba(220,220,220,1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [65, 59, 80, 81, 56, 55, 40],
+                spanGaps: false,
+            }
+        ]
+    },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+  });
 
     // this.seanceService.seanceSubject.subscribe(res => {
     //   this.seance = res;
